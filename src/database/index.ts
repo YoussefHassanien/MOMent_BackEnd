0fd7135c74fd7b65756515188fffdcbc345f027b
Youssef Hassanien <Youssefhassanein17@gmail.com>
2025-10-11 20:05:55 +0300

    Redeploy

--- src/database/index.ts
+++ src/database/index.ts
@@ -6,4 +6,3 @@ export { Surgery } from './entities/surgery.entity';
 export { User } from './entities/user.entity';
 export { VitalSignType } from './entities/vital-sign-type.entity';
 export { VitalSign } from './entities/vital-sign.entity';
-export { Surgery } from './entities/surgery.entity';
